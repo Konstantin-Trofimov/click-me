374dbbf1f7125381a5b04f8e15c6757a97b90a44
Konstantin Trofimov <const.trofimov@gmail.com>
2024-04-02 12:05:00 +0300

    init

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button
+      v-if="count === deep"
+      @click="emit('click')"
+    >
+      click me
+    </button>
+    <ChildComponent
+      v-else 
+      :deep="deep + 1" 
+      :count="count"
+      @click="emit('click')"
+    />
+  </div>
+</template>
+
+<script setup>
+import ChildComponent from './ChildComponent.vue'
+
+const props = defineProps({
+  count: { type: Number, required: true },
+  deep: { type: Number, default: 0 },
+})
+const emit = defineEmits(['click'])
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid red;
+    width: 95%;
+    height: 95%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <ChildComponent :count="count" @click="updateCounter"/>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ChildComponent from './ChildComponent.vue'
+
+const count = ref(0)
+const updateCounter = () => {
+  count.value++
+}
+</script>
